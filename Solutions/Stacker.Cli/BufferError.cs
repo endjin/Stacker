af617a440a366ca8c85efa34efd3f2f4a86ea65c
Howard van Rooijen <Howard.vanRooijen@endjin.com>
2020-02-29 21:49:44 +0000

    Read, deserialize, and display the buffer error message

---
+++ Solutions/Stacker.Cli/BufferError.cs
@@ -0,0 +1,24 @@
+﻿// <copyright file="BufferError.cs" company="Endjin Limited">
+// Copyright (c) Endjin Limited. All rights reserved.
+// </copyright>
+
+namespace Stacker.Cli
+{
+    using System.Collections.Generic;
+    using Newtonsoft.Json;
+
+    public class BufferError
+    {
+        [JsonProperty("success")]
+        public bool Success { get; set; }
+
+        [JsonProperty("message")]
+        public string Message { get; set; }
+
+        [JsonProperty("code")]
+        public int Code { get; set; }
+
+        [JsonProperty("errored_profiles")]
+        public IEnumerable<Profiles> ErroredProfiles { get; set; }
+    }
+}
---
+++ Solutions/Stacker.Cli/Profiles.cs
@@ -0,0 +1,14 @@
+﻿// <copyright file="Profiles.cs" company="Endjin Limited">
+// Copyright (c) Endjin Limited. All rights reserved.
+// </copyright>
+
+namespace Stacker.Cli
+{
+    using Newtonsoft.Json;
+
+    public class Profiles
+    {
+        [JsonProperty("profile_id")]
+        public string ProfileId { get; set; }
+    }
+}
